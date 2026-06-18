278e4eecfaa6d32f036fdc42cf13a3c61420bcbe
learningdev-student <learningdev.student@gmail.com>
2026-06-18 20:39:36 +0300

    Code formatted

--- test_login.py
+++ test_login.py
@@ -1,28 +1,10 @@
 from login import login
 
 
-
-
-
-
-
 def test_login():
-
-
-
 
     username = "testuser"
     password = "testpassword"
     expected_output = f"Login successful for user: {username} Please check your email."
-
-
-
-
-
-
-
-
-
-
 
     assert login(username, password) == expected_output
