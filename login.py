dfa8be584966700e0191b404f325895c36d281d4
learningdev-student <learningdev.student@gmail.com>
2026-06-18 20:38:31 +0300

    Testing pre-commit hook

--- login.py
+++ login.py
@@ -1,3 +1,8 @@
 def login(username, password):
 
+
+
+
+
+
     return f"Login successful for user: {username} Please check your email."
--- test_login.py
+++ test_login.py
@@ -1,10 +1,28 @@
 from login import login
 
 
+
+
+
+
+
 def test_login():
+
+
+
 
     username = "testuser"
     password = "testpassword"
     expected_output = f"Login successful for user: {username} Please check your email."
+
+
+
+
+
+
+
+
+
+
 
     assert login(username, password) == expected_output
